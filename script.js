80d70cd930eb9f5d07c1c271f3504b67150cf763
shahidul shohid <shahidulislamshohid7gmail.com>
2022-11-28 18:16:20 +0600

    updated

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
-
 const fs = require("fs");
-
 const http = require("http");
-
 const url = require("url");
 
-const server=http.createServer((req,res)=>{
+const server = http.createServer((req,res)=>{
 
-const urlObject=url.parse(req.url,true);
+    const urlObject = url.parse(req.url,true);
+    const fileName = `${__dirname}/${urlObject.pathname}`;
 
-const fileName=`${__dirname}/${urlObject.pathname}`;
+    fs.readFile(fileName,(error,data)=>{
 
-fs.writeFile(fileName,(error,data)=>{
+        if(error){
 
-    if(error){
+            res.writeHead(404,{"content-Type":"text/html"});
+            res.end(`<h1>404 page not found`);
+        }
 
-        res.writeHead(404,{"content-Type":"text/html"});
-
-        res.end(`404 page not found`);
-    }
-
-    res.writeHead(200,{"content-Type":"text/html"});
-
-    res.write(data);
-
-    res.end;
+        res.writeHead(200,{"content-Type":"text/html"});
+        res.write(data);
+        res.end();
+    })
 
 
-})
 
 })
 
 server.listen(8000,()=>{
 
-    console.log("Server is listening at port 8000");
+    console.log(`server is running at port 800`);
 })

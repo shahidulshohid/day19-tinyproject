6793c52acd58ec7d1d361eb8bf14e08c83398733
shahidul shohid <shahidulislamshohid7gmail.com>
2022-11-28 07:31:55 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const fs = require("fs");
+
+const http = require("http");
+
+const url = require("url");
+
+const server=http.createServer((req,res)=>{
+
+const urlObject=url.parse(req.url,true);
+
+const fileName=`${__dirname}/${urlObject.pathname}`;
+
+fs.writeFile(fileName,(error,data)=>{
+
+    if(error){
+
+        res.writeHead(404,{"content-Type":"test/html"});
+
+        res.end(`404 page not found`);
+    }
+
+    res.writeHead(200,{"content-Type":"test/html"});
+
+    res.write(data);
+    
+    res.end;
+
+
+})
+
+})
+
+server.listen(8000,()=>{
+
+    console.log("Server is listening at port 8000");
+})
